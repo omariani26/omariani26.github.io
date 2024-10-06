6e0e7364fac9c113d2e2ac3128dae0ed22bd9c55
Owen Mariani <mariani.owen@gmail.com>
2024-10-06 18:37:55 -0400

    added navbar

--- src/navbar/menu/menu.component.ts
+++ src/navbar/menu/menu.component.ts
@@ -1,17 +1,31 @@
 import { ChangeDetectionStrategy, Component } from "@angular/core";
 import { NbActionsModule, NbIconConfig, NbThemeModule } from "@nebular/theme";
+import { NbIconModule } from "@nebular/theme";
+import { CommonModule } from "@angular/common";
 
 @Component({
   selector: "nb-menu",
   changeDetection: ChangeDetectionStrategy.OnPush,
   standalone: true,
   templateUrl: "./menu.component.html",
-  imports: [NbThemeModule, NbActionsModule],
-  styleUrl: "./menu.component.css",
+  imports: [
+    NbThemeModule,
+    NbActionsModule,
+    NbIconModule,
+    NbIconModule,
+    CommonModule,
+  ],
+  styleUrls: ["./menu.component.css"],
 })
 export class MenuComponent {
   disabledIconConfig: NbIconConfig = {
     icon: "settings-2-outline",
     pack: "eva",
   };
+  menuItems = [
+    { name: "ABOUT" },
+    { name: "SKILLS" },
+    { name: "PROJECTS" },
+    { name: "EXPERIENCE" },
+  ];
 }
